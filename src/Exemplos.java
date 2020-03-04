c358cd0811d06c1031f6465311382ae052d37524
Kleber <kleber.brito@etec.sp.gov.br>
2020-03-04 19:56:26 -0300

    Correção do repositório

--- src/Exemplos.java
+++ src/Exemplos.java
@@ -1,4 +1,17 @@
 
 public class Exemplos {
+	public static void main(String[] args) {
+		int idade = 17;
+		
+		if (idade >= 18) {
+			
+			System.out.println("MAIOR DE IDADE");
+		
+		} else {
+			
+			System.out.println("MENOR DE IDADE");
+
+		}
+	}
 
 }
